2b14a15991e5b6b1a1b07f6a525778cd69310683
吴律 <wulv@xiaoyouzi.com>
2016-07-30 12:10:44 +0800

    tow sum

--- 1. Two Sum/wulv.js
+++ 1. Two Sum/wulv.js
@@ -1,8 +1,22 @@
 /**
+Example:
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-
+  var len = nums.length,res = [];
+  for (var i = 0; i < len; i++) {
+    for (var j = i + 1; j < len; j++) {
+      if (nums[i] + nums[j] === target) {
+        res.push(i);
+        res.push(j);
+        return res;
+      }
+    }
+  }
 };
